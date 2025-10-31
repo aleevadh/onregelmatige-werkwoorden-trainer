89d7b5e195cf26848f63d93f73e667eba53e49b7
Alessia (ง •̀_•́)ง 𓅭𓅰๋࣭ ⭑ <alessia.vadacca18@gmail.com>
2025-10-31 18:46:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,946 @@
+const verbs = [
+  {
+    infinitief: "bakken",
+    imperfectum_sg: "bakte",
+    imperfectum_pl: "bakten",
+    perfectum: "gebakken",
+  },
+  {
+    infinitief: "bederven",
+    imperfectum_sg: "bedierf",
+    imperfectum_pl: "bedierven",
+    perfectum: "bedorven",
+  },
+  {
+    infinitief: "bedriegen",
+    imperfectum_sg: "bedroog",
+    imperfectum_pl: "bedrogen",
+    perfectum: "bedrogen",
+  },
+  {
+    infinitief: "beginnen",
+    imperfectum_sg: "begon",
+    imperfectum_pl: "begonnen",
+    perfectum: "begonnen",
+  },
+  {
+    infinitief: "begrijpen",
+    imperfectum_sg: "begreep",
+    imperfectum_pl: "begrepen",
+    perfectum: "begrepen",
+  },
+  {
+    infinitief: "bevallen",
+    imperfectum_sg: "beviel",
+    imperfectum_pl: "bevielen",
+    perfectum: "bevallen",
+  },
+  {
+    infinitief: "bewegen",
+    imperfectum_sg: "bewoog",
+    imperfectum_pl: "bewogen",
+    perfectum: "bewogen",
+  },
+  {
+    infinitief: "bezoeken",
+    imperfectum_sg: "bezocht",
+    imperfectum_pl: "bezochten",
+    perfectum: "bezocht",
+  },
+  {
+    infinitief: "bidden",
+    imperfectum_sg: "bad",
+    imperfectum_pl: "baden",
+    perfectum: "gebeden",
+  },
+  {
+    infinitief: "bieden",
+    imperfectum_sg: "bood",
+    imperfectum_pl: "boden",
+    perfectum: "geboden",
+  },
+  {
+    infinitief: "bijten",
+    imperfectum_sg: "beet",
+    imperfectum_pl: "beten",
+    perfectum: "gebeten",
+  },
+  {
+    infinitief: "binden",
+    imperfectum_sg: "bond",
+    imperfectum_pl: "bonden",
+    perfectum: "gebonden",
+  },
+  {
+    infinitief: "blazen",
+    imperfectum_sg: "blies",
+    imperfectum_pl: "bliezen",
+    perfectum: "geblazen",
+  },
+  {
+    infinitief: "blijken",
+    imperfectum_sg: "bleek",
+    imperfectum_pl: "bleken",
+    perfectum: "gebleken",
+  },
+  {
+    infinitief: "blijven",
+    imperfectum_sg: "bleef",
+    imperfectum_pl: "bleven",
+    perfectum: "gebleven",
+  },
+  {
+    infinitief: "braden",
+    imperfectum_sg: "braadde",
+    imperfectum_pl: "braadden",
+    perfectum: "gebraden",
+  },
+  {
+    infinitief: "breken",
+    imperfectum_sg: "brak",
+    imperfectum_pl: "braken",
+    perfectum: "gebroken",
+  },
+  {
+    infinitief: "brengen",
+    imperfectum_sg: "bracht",
+    imperfectum_pl: "brachten",
+    perfectum: "gebracht",
+  },
+  {
+    infinitief: "buigen",
+    imperfectum_sg: "boog",
+    imperfectum_pl: "bogen",
+    perfectum: "gebogen",
+  },
+  {
+    infinitief: "denken",
+    imperfectum_sg: "dacht",
+    imperfectum_pl: "dachten",
+    perfectum: "gedacht",
+  },
+  {
+    infinitief: "doen",
+    imperfectum_sg: "deed",
+    imperfectum_pl: "deden",
+    perfectum: "gedaan",
+  },
+  {
+    infinitief: "dragen",
+    imperfectum_sg: "droeg",
+    imperfectum_pl: "droegen",
+    perfectum: "gedragen",
+  },
+  {
+    infinitief: "drijven",
+    imperfectum_sg: "dreef",
+    imperfectum_pl: "dreven",
+    perfectum: "gedreven",
+  },
+  {
+    infinitief: "drinken",
+    imperfectum_sg: "dronk",
+    imperfectum_pl: "dronken",
+    perfectum: "gedronken",
+  },
+  {
+    infinitief: "duiken",
+    imperfectum_sg: "dook",
+    imperfectum_pl: "doken",
+    perfectum: "gedoken",
+  },
+  {
+    infinitief: "dwingen",
+    imperfectum_sg: "dwong",
+    imperfectum_pl: "dwongen",
+    perfectum: "gedwongen",
+  },
+  {
+    infinitief: "ervaren",
+    imperfectum_sg: "ervoer",
+    imperfectum_pl: "ervoeren",
+    perfectum: "ervaren",
+  },
+  {
+    infinitief: "eten",
+    imperfectum_sg: "at",
+    imperfectum_pl: "aten",
+    perfectum: "gegeten",
+  },
+  {
+    infinitief: "fluiten",
+    imperfectum_sg: "floot",
+    imperfectum_pl: "floten",
+    perfectum: "gefloten",
+  },
+
+  {
+  infinitief: "gaan",
+  imperfectum_sg: "ging",
+  imperfectum_pl: "gingen",
+  perfectum: "gegaan",
+},
+{
+  infinitief: "genezen",
+  imperfectum_sg: "genas",
+  imperfectum_pl: "genazen",
+  perfectum: "genezen",
+},
+
+{
+  infinitief: "genieten",
+  imperfectum_sg: "genoot",
+  imperfectum_pl: "genoten",
+  perfectum: "genoten",
+},
+{
+  infinitief: "geven",
+  imperfectum_sg: "gaf",
+  imperfectum_pl: "gaven",
+  perfectum: "gegeven",
+},
+{
+  infinitief: "gieten",
+  imperfectum_sg: "goot",
+  imperfectum_pl: "goten",
+  perfectum: "gegoten",
+},
+{
+  infinitief: "glijden",
+  imperfectum_sg: "gleed",
+  imperfectum_pl: "gleden",
+  perfectum: "gegleden",
+},
+{
+  infinitief: "glimmen",
+  imperfectum_sg: "glom",
+  imperfectum_pl: "glommen",
+  perfectum: "geglommen",
+},
+{
+  infinitief: "graven",
+  imperfectum_sg: "groef",
+  imperfectum_pl: "groeven",
+  perfectum: "gegraven",
+},
+{
+  infinitief: "hangen",
+  imperfectum_sg: "hing",
+  imperfectum_pl: "hingen",
+  perfectum: "gehangen",
+},
+{
+  infinitief: "hebben",
+  imperfectum_sg: "had",
+  imperfectum_pl: "hadden",
+  perfectum: "gehad",
+},
+{
+  infinitief: "helpen",
+  imperfectum_sg: "hielp",
+  imperfectum_pl: "hielpen",
+  perfectum: "geholpen",
+},
+{
+  infinitief: "heten",
+  imperfectum_sg: "heette",
+  imperfectum_pl: "heetten",
+  perfectum: "geheten",
+},
+{
+  infinitief: "houden",
+  imperfectum_sg: "hield",
+  imperfectum_pl: "hielden",
+  perfectum: "gehouden",
+},
+{
+  infinitief: "kiezen",
+  imperfectum_sg: "koos",
+  imperfectum_pl: "kozen",
+  perfectum: "gekozen",
+},
+{
+  infinitief: "kijken",
+  imperfectum_sg: "keek",
+  imperfectum_pl: "keken",
+  perfectum: "gekeken",
+},
+{
+  infinitief: "klimmen",
+  imperfectum_sg: "klom",
+  imperfectum_pl: "klommen",
+  perfectum: "geklommen",
+},
+{
+  infinitief: "klinken",
+  imperfectum_sg: "klonk",
+  imperfectum_pl: "klonken",
+  perfectum: "geklonken",
+},
+{
+  infinitief: "knijpen",
+  imperfectum_sg: "kneep",
+  imperfectum_pl: "knepen",
+  perfectum: "geknepen",
+},
+{
+  infinitief: "komen",
+  imperfectum_sg: "kwam",
+  imperfectum_pl: "kwamen",
+  perfectum: "gekomen",
+},
+
+{
+  infinitief: "kopen",
+  imperfectum_sg: "kocht",
+  imperfectum_pl: "kochten",
+  perfectum: "gekocht",
+},
+{
+  infinitief: "krijgen",
+  imperfectum_sg: "kreeg",
+  imperfectum_pl: "kregen",
+  perfectum: "gekregen",
+},
+{
+  infinitief: "krimpen",
+  imperfectum_sg: "kromp",
+  imperfectum_pl: "krompen",
+  perfectum: "gekrompen",
+},
+{
+  infinitief: "kruipen",
+  imperfectum_sg: "kroop",
+  imperfectum_pl: "kropen",
+  perfectum: "gekropen",
+},
+{
+  infinitief: "kunnen",
+  imperfectum_sg: "kon",
+  imperfectum_pl: "konden",
+  perfectum: "gekund",
+},
+{
+  infinitief: "lachen",
+  imperfectum_sg: "lachte",
+  imperfectum_pl: "lachten",
+  perfectum: "gelachen",
+},
+{
+  infinitief: "laden",
+  imperfectum_sg: "laadde",
+  imperfectum_pl: "laadden",
+  perfectum: "geladen",
+},
+{
+  infinitief: "laten",
+  imperfectum_sg: "liet",
+  imperfectum_pl: "lieten",
+  perfectum: "gelaten",
+},
+{
+  infinitief: "lezen",
+  imperfectum_sg: "las",
+  imperfectum_pl: "lazen",
+  perfectum: "gelezen",
+},
+{
+  infinitief: "liegen",
+  imperfectum_sg: "loog",
+  imperfectum_pl: "logen",
+  perfectum: "gelogen",
+},
+{
+  infinitief: "liggen",
+  imperfectum_sg: "lag",
+  imperfectum_pl: "lagen",
+  perfectum: "gelegen",
+},
+{
+  infinitief: "lijden",
+  imperfectum_sg: "leed",
+  imperfectum_pl: "leden",
+  perfectum: "gelopen",
+},
+{
+  infinitief: "lijken",
+  imperfectum_sg: "leek",
+  imperfectum_pl: "leken",
+  perfectum: "geleken",
+},
+{
+  infinitief: "lopen",
+  imperfectum_sg: "liep",
+  imperfectum_pl: "liepen",
+  perfectum: "gelopen",
+},
+
+{
+  infinitief: "meten",
+  imperfectum_sg: "mat",
+  imperfectum_pl: "maten",
+  perfectum: "gemeten",
+},
+{
+  infinitief: "moeten",
+  imperfectum_sg: "moest",
+  imperfectum_pl: "moesten",
+  perfectum: "gemoeten",
+},
+{
+  infinitief: "mogen",
+  imperfectum_sg: "mocht",
+  imperfectum_pl: "mochten",
+  perfectum: "gemogen",
+},
+{
+  infinitief: "nemen",
+  imperfectum_sg: "nam",
+  imperfectum_pl: "namen",
+  perfectum: "genomen",
+},
+{
+  infinitief: "ontbijten",
+  imperfectum_sg: "ontbeet",
+  imperfectum_pl: "ontbeten",
+  perfectum: "ontbeten",
+},
+{
+  infinitief: "ontwerpen",
+  imperfectum_sg: "ontwierp",
+  imperfectum_pl: "ontwierpen",
+  perfectum: "ontworpen",
+},
+{
+  infinitief: "overlijden",
+  imperfectum_sg: "overleed",
+  imperfectum_pl: "overleden",
+  perfectum: "overleden",
+},
+{
+  infinitief: "raden",
+  imperfectum_sg: "raadde",
+  imperfectum_pl: "raadden",
+  perfectum: "geraden",
+},
+{
+  infinitief: "rijden",
+  imperfectum_sg: "reed",
+  imperfectum_pl: "reden",
+  perfectum: "gereden",
+},
+{
+  infinitief: "roepen",
+  imperfectum_sg: "riep",
+  imperfectum_pl: "riepen",
+  perfectum: "geroepen",
+},
+{
+  infinitief: "ruiken",
+  imperfectum_sg: "rook",
+  imperfectum_pl: "roken",
+  perfectum: "geroken",
+},
+{
+  infinitief: "scheiden",
+  imperfectum_sg: "scheidde",
+  imperfectum_pl: "scheidden",
+  perfectum: "gescheiden",
+},
+{
+  infinitief: "schelden",
+  imperfectum_sg: "schold",
+  imperfectum_pl: "scholden",
+  perfectum: "gescholden",
+},
+{
+  infinitief: "schenken",
+  imperfectum_sg: "schonk",
+  imperfectum_pl: "schonken",
+  perfectum: "geschonken",
+},
+{
+  infinitief: "scheren",
+  imperfectum_sg: "schoor",
+  imperfectum_pl: "schoren",
+  perfectum: "geschoren",
+},
+{
+  infinitief: "schieten",
+  imperfectum_sg: "schoot",
+  imperfectum_pl: "schoten",
+  perfectum: "geschoten",
+},
+{
+  infinitief: "schijnen",
+  imperfectum_sg: "scheen",
+  imperfectum_pl: "schenen",
+  perfectum: "geschenen",
+},
+{
+  infinitief: "schrijven",
+  imperfectum_sg: "schreef",
+  imperfectum_pl: "schreven",
+  perfectum: "geschreven",
+},
+{
+  infinitief: "schrikken",
+  imperfectum_sg: "schrok",
+  imperfectum_pl: "schrokken",
+  perfectum: "geschrokken",
+},
+{
+  infinitief: "schuiven",
+  imperfectum_sg: "schoof",
+  imperfectum_pl: "schoven",
+  perfectum: "geschoven",
+},
+{
+  infinitief: "slaan",
+  imperfectum_sg: "sloeg",
+  imperfectum_pl: "sloegen",
+  perfectum: "geslagen",
+},
+{
+  infinitief: "slapen",
+  imperfectum_sg: "sliep",
+  imperfectum_pl: "sliepen",
+  perfectum: "geslapen",
+},
+{
+  infinitief: "slijpen",
+  imperfectum_sg: "sleep",
+  imperfectum_pl: "slepen",
+  perfectum: "geslepen",
+},
+{
+  infinitief: "sluipen",
+  imperfectum_sg: "sloop",
+  imperfectum_pl: "slopen",
+  perfectum: "geslopen",
+},
+{
+  infinitief: "sluiten",
+  imperfectum_sg: "sloot",
+  imperfectum_pl: "sloten",
+  perfectum: "gesloten",
+},
+{
+  infinitief: "smelten",
+  imperfectum_sg: "smolt",
+  imperfectum_pl: "smolten",
+  perfectum: "gesmolten",
+},
+{
+  infinitief: "snijden",
+  imperfectum_sg: "sneed",
+  imperfectum_pl: "sneden",
+  perfectum: "gesneden",
+},
+{
+  infinitief: "snuiten",
+  imperfectum_sg: "snoot",
+  imperfectum_pl: "snoten",
+  perfectum: "gesnoten",
+},
+{
+  infinitief: "snuiven",
+  imperfectum_sg: "snoof",
+  imperfectum_pl: "snoven",
+  perfectum: "gesnoven",
+},
+{
+  infinitief: "spijten",
+  imperfectum_sg: "speet",
+  imperfectum_pl: "speet",
+  perfectum: "gespeten",
+},
+{
+  infinitief: "spreken",
+  imperfectum_sg: "sprak",
+  imperfectum_pl: "spraken",
+  perfectum: "gesproken",
+},
+{
+  infinitief: "springen",
+  imperfectum_sg: "sprong",
+  imperfectum_pl: "sprongen",
+  perfectum: "gesprongen",
+},
+{
+  infinitief: "spuiten",
+  imperfectum_sg: "spoot",
+  imperfectum_pl: "spoten",
+  perfectum: "gespoten",
+},
+{
+  infinitief: "staan",
+  imperfectum_sg: "stond",
+  imperfectum_pl: "stonden",
+  perfectum: "gestaan",
+},
+{
+  infinitief: "steken",
+  imperfectum_sg: "stak",
+  imperfectum_pl: "staken",
+  perfectum: "gestoken",
+},
+{
+  infinitief: "stelen",
+  imperfectum_sg: "stal",
+  imperfectum_pl: "stalen",
+  perfectum: "gestolen",
+},
+{
+  infinitief: "sterven",
+  imperfectum_sg: "stierf",
+  imperfectum_pl: "stierven",
+  perfectum: "gestorven",
+},
+{
+  infinitief: "stijgen",
+  imperfectum_sg: "steeg",
+  imperfectum_pl: "stegen",
+  perfectum: "gestegen",
+},
+{
+  infinitief: "stinken",
+  imperfectum_sg: "stonk",
+  imperfectum_pl: "stonken",
+  perfectum: "gestonken",
+},
+{
+  infinitief: "strijden",
+  imperfectum_sg: "streed",
+  imperfectum_pl: "streden",
+  perfectum: "gestreden",
+},
+{
+  infinitief: "strijken",
+  imperfectum_sg: "streek",
+  imperfectum_pl: "streken",
+  perfectum: "gestreken",
+},
+{
+  infinitief: "trekken",
+  imperfectum_sg: "trok",
+  imperfectum_pl: "trokken",
+  perfectum: "getrokken",
+},
+{
+  infinitief: "vallen",
+  imperfectum_sg: "viel",
+  imperfectum_pl: "vielen",
+  perfectum: "gevallen",
+},
+{
+  infinitief: "vangen",
+  imperfectum_sg: "ving",
+  imperfectum_pl: "vingen",
+  perfectum: "gevangen",
+},
+{
+  infinitief: "varen",
+  imperfectum_sg: "voer",
+  imperfectum_pl: "voeren",
+  perfectum: "gevaren",
+},
+{
+  infinitief: "vechten",
+  imperfectum_sg: "vocht",
+  imperfectum_pl: "vochten",
+  perfectum: "gevochten",
+},
+{
+  infinitief: "verbieden",
+  imperfectum_sg: "verbood",
+  imperfectum_pl: "verboden",
+  perfectum: "verboden",
+},
+{
+  infinitief: "verdwijnen",
+  imperfectum_sg: "verdween",
+  imperfectum_pl: "verdwenen",
+  perfectum: "verdwenen",
+},
+{
+  infinitief: "vergelijken",
+  imperfectum_sg: "vergeleek",
+  imperfectum_pl: "vergeleken",
+  perfectum: "vergeleken",
+},
+{
+  infinitief: "vergeten",
+  imperfectum_sg: "vergat",
+  imperfectum_pl: "vergaten",
+  perfectum: "vergeten",
+},
+{
+  infinitief: "verliezen",
+  imperfectum_sg: "verloor",
+  imperfectum_pl: "verloren",
+  perfectum: "verloren",
+},
+{
+  infinitief: "vermijden",
+  imperfectum_sg: "vermeed",
+  imperfectum_pl: "vermijden",
+  perfectum: "vermijden",
+},
+{
+  infinitief: "verraden",
+  imperfectum_sg: "verraadde",
+  imperfectum_pl: "verraadden",
+  perfectum: "verraden",
+},
+{
+  infinitief: "vertrekken",
+  imperfectum_sg: "vertrok",
+  imperfectum_pl: "vertrokken",
+  perfectum: "vertrokken",
+},
+{
+  infinitief: "verwijten",
+  imperfectum_sg: "verweet",
+  imperfectum_pl: "verweten",
+  perfectum: "verweten",
+},
+{
+  infinitief: "verzinnen",
+  imperfectum_sg: "verzon",
+  imperfectum_pl: "verzonnen",
+  perfectum: "verzonnen",
+},
+{
+  infinitief: "vinden",
+  imperfectum_sg: "vond",
+  imperfectum_pl: "vonden",
+  perfectum: "gevonden",
+},
+{
+  infinitief: "vliegen",
+  imperfectum_sg: "vloog",
+  imperfectum_pl: "vlogen",
+  perfectum: "gevlogen",
+},
+{
+  infinitief: "vouwen",
+  imperfectum_sg: "vouwde",
+  imperfectum_pl: "vouwden",
+  perfectum: "gevouwen",
+},
+{
+  infinitief: "vragen",
+  imperfectum_sg: "vroeg",
+  imperfectum_pl: "vroegen",
+  perfectum: "gevraagd",
+},
+{
+  infinitief: "vriezen",
+  imperfectum_sg: "vroor",
+  imperfectum_pl: "vroren",
+  perfectum: "gevroren",
+},
+{
+  infinitief: "wassen",
+  imperfectum_sg: "waste",
+  imperfectum_pl: "wasten",
+  perfectum: "gewassen",
+},
+{
+  infinitief: "wegen",
+  imperfectum_sg: "woog",
+  imperfectum_pl: "wogen",
+  perfectum: "gewogen",
+},
+{
+  infinitief: "werpen",
+  imperfectum_sg: "wierp",
+  imperfectum_pl: "wierpen",
+  perfectum: "geworpen",
+},
+{
+  infinitief: "weten",
+  imperfectum_sg: "wist",
+  imperfectum_pl: "wisten",
+  perfectum: "geweten",
+},
+{
+  infinitief: "wijzen",
+  imperfectum_sg: "wees",
+  imperfectum_pl: "wezen",
+  perfectum: "gewezen",
+},
+{
+  infinitief: "willen",
+  imperfectum_sg: "wilde",
+  imperfectum_pl: "wilden",
+  perfectum: "gewild",
+},
+{
+  infinitief: "winnen",
+  imperfectum_sg: "won",
+  imperfectum_pl: "wonnen",
+  perfectum: "gewonnen",
+},
+{
+  infinitief: "worden",
+  imperfectum_sg: "werd",
+  imperfectum_pl: "werden",
+  perfectum: "geworden",
+},
+{
+  infinitief: "wrijven",
+  imperfectum_sg: "wreef",
+  imperfectum_pl: "wreven",
+  perfectum: "gewreven",
+},
+{
+  infinitief: "zeggen",
+  imperfectum_sg: "zei",
+  imperfectum_pl: "zeiden",
+  perfectum: "gezegd",
+},
+{
+  infinitief: "zenden",
+  imperfectum_sg: "zond",
+  imperfectum_pl: "zonden",
+  perfectum: "gezonden",
+},
+{
+  infinitief: "zien",
+  imperfectum_sg: "zag",
+  imperfectum_pl: "zagen",
+  perfectum: "gezien",
+},
+{
+  infinitief: "zijn",
+  imperfectum_sg: "was",
+  imperfectum_pl: "waren",
+  perfectum: "geweest",
+},
+{
+  infinitief: "zingen",
+  imperfectum_sg: "zong",
+  imperfectum_pl: "zongen",
+  perfectum: "gezongen",
+},
+{
+  infinitief: "zinken",
+  imperfectum_sg: "zonk",
+  imperfectum_pl: "zonken",
+  perfectum: "gezonken",
+},
+{
+  infinitief: "zitten",
+  imperfectum_sg: "zat",
+  imperfectum_pl: "zaten",
+  perfectum: "gezeten",
+},
+{
+  infinitief: "zoeken",
+  imperfectum_sg: "zocht",
+  imperfectum_pl: "zochten",
+  perfectum: "gezocht",
+},
+{
+  infinitief: "zuigen",
+  imperfectum_sg: "zoog",
+  imperfectum_pl: "zogen",
+  perfectum: "gezogen",
+},
+{
+  infinitief: "zullen",
+  imperfectum_sg: "zou",
+  imperfectum_pl: "zouden",
+  perfectum: "",
+},
+{
+  infinitief: "zwemmen",
+  imperfectum_sg: "zwom",
+  imperfectum_pl: "zwommen",
+  perfectum: "gezwommen",
+},
+{
+  infinitief: "zwijgen",
+  imperfectum_sg: "zweeg",
+  imperfectum_pl: "zwegen",
+  perfectum: "gezwegen",
+},
+];
+
+
+const categories = [
+  "infinitief",
+  "imperfectum_sg",
+  "imperfectum_pl",
+  "perfectum",
+];
+
+const categoryLabels = {
+  infinitief: "Infinitief",
+  imperfectum_sg: "Imperfectum enkelvoud",
+  imperfectum_pl: "Imperfectum meervoud",
+  perfectum: "Perfectum",
+};
+
+let currentVerb, fromCategory, toCategory;
+let awaitingCheck = true;
+
+function nextQuestion() {
+  document.getElementById("feedback").textContent = "";
+  document.getElementById("answer").value = "";
+
+  currentVerb = verbs[Math.floor(Math.random() * verbs.length)];
+  fromCategory = "infinitief";
+
+  const targetCategories = ["imperfectum_sg", "imperfectum_pl", "perfectum"];
+  toCategory =
+    targetCategories[Math.floor(Math.random() * targetCategories.length)];
+
+  document.getElementById(
+    "question"
+  ).innerHTML = `<u>Werkwoorden</u> > ${currentVerb[fromCategory]}<br><u>Conjugeer</u> in > ${categoryLabels[toCategory]}`;
+
+  awaitingCheck = true;
+  document.getElementById("button").textContent = "✔";
+}
+
+function checkAnswer() {
+  const userAnswer = document.getElementById("answer").value.trim();
+  const correctAnswer = currentVerb[toCategory];
+  const feedback = document.getElementById("feedback");
+
+  if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    feedback.textContent = "Correct!";
+  } else {
+    feedback.textContent = `No, het juiste antwoord is: ${correctAnswer}`;
+  }
+
+  awaitingCheck = false;
+  document.getElementById("button").textContent = "Next";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("button");
+
+  btn.addEventListener("click", () => {
+    if (awaitingCheck) {
+      checkAnswer();
+    } else {
+      nextQuestion();
+    }
+  });
+
+  document.getElementById("answer").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      if (awaitingCheck) {
+        checkAnswer();
+      } else {
+        nextQuestion();
+      }
+    }
+  });
+
+  nextQuestion();
+});
